adae44f5b722cff36f70dafc86741c68a43c70ad
Ankita Sarkar <ankitasarkar.sectionn@gmail.com>
2025-12-14 11:10:10 +0530

    feat: implement main application layout and styling, add input functionality for user greeting

--- REACT JS/styling-css/src/App.jsx
+++ REACT JS/styling-css/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import React, {useState} from "react";
+import ReactDOM from "react-dom/client";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [name, setName] = useState("");
+
+  const showOutput = () => {
+    const outputDiv = document.querySelector(".output");
+    outputDiv.textContent = `Hello, ${name}! How are you today?`;
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="main">
+      <h1>Demo of using CSS with React</h1>
+      <div className="input">
+        <input 
+          type="text" 
+          placeholder="Enter your full name" 
+          value={name} 
+          onChange={(e) => setName(e.target.value)}  
+        />
+        <button onClick={showOutput}>Submit</button>
+        <div className="output"></div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
