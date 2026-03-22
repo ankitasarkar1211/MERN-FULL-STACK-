8a753ab3f75482b2dcccdd65d9c2356dd2a0301a
Ankita Sarkar <ankitasarkar.sectionn@gmail.com>
2026-03-22 08:36:15 +0530

    Add initial frontend setup with React, Tailwind CSS, and Vite
    - Created main entry point (main.jsx) for React application.
    - Added Tailwind CSS configuration (tailwind.config.js) and included base styles in index.css.
    - Introduced SVG assets for React and Vite logos.
    - Configured Vite for React and Babel support in vite.config.js.

---
+++ EXPRESS and REACT/authentication/frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+
+      {/* Navbar */}
+      <div className="bg-blue-600 text-white py-4 shadow-md">
+        <h1 className="text-center text-2xl font-bold">
+          Study Task Manager
+        </h1>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
